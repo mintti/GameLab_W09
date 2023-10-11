8239dcea5470588c5b1ddfd8272a8d05d0605ae7
mintti <mintti97@gmail.com>
2023-10-11 15:11:25 +0900

    오브젝트 초안

---
+++ Assets/Modules/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+
+    IEnumerator GameFlow()
+    {
+        
+        // 
+
+        yield return null;
+    }
+}
